d6b2f698d22c00719849fa606f34d60503e7d2cf
Abhiraj <Abhiraj@gmail.com>
2025-12-15 16:59:56 +0530

    Write Create Course Code

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -15,6 +15,7 @@ import ForgetPassword from "./pages/ForgetPassword";
 import EditProfile from "./pages/EditProfile";
 import Dashboard from "./pages/Educator/Dashboard";
 import Courses from "./pages/Educator/Courses";
+import CreateCourses from "./pages/Educator/CreateCourses";
 
 
 function App(){
@@ -34,6 +35,7 @@ function App(){
       <Route path='/editprofile' element={userData ? <EditProfile/> : <Navigate to={"/signup"}/>}/> 
       <Route path='/dashboard' element={userData?.role === "educator" ? <Dashboard/> : <Navigate to={"/signup"}/>}/>
       <Route path='/courses' element={userData?.role === "educator" ? <Courses/> : <Navigate to={"/signup"}/>}/> 
+      <Route path='/createcourse' element={userData?.role === "educator" ? <CreateCourses/> : <Navigate to={"/signup"}/>}/> 
  
 
       
--- fronted/src/pages/Educator/CreateCourses.jsx
+++ fronted/src/pages/Educator/CreateCourses.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 function CreateCourses() {
+  const navigate = useNavigate()
   return (
-    <div>
+    <div className='min-h-screen flex items-center justify-center bg-gray-100 px-4 py-10'>
+      <div className='max-w-xl w-[600px] mx-auto p-6 bg-white shadow-md rounded-md mt-10 relative'>
+        <FaArrowLeftLong className='top-[8%] absolute left-[5%] w-[22px] h-[22px] cursor-pointer' onClick={()=>navigate("/courses")}/>
+          <h2 className='text-2xl font-semibold mb-6 text-center'>Create Courses</h2>
+
+          <form className='space-y-5'>
+            <div>
+              <label htmlFor="title" className='block text-sm font-medium text-gray-700 mb-1'>Course Title</label>
+              <input type="text" id='title' placeholder='Enter Course title' className='w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[black]'/>
+            </div>
+
+            <div>
+              <label htmlFor="cat" className='block text-sm font-medium text-gray-700 mb-1'>Course Category</label>
+              <select id="cat" className='w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[black] cursor-pointer'>
+                <option value="">Select Category</option>
+                <option value="App Development">App Development</option>
+                <option value="AI Tools">AI Tools</option>
+                <option value="AI/ML">AI/ML</option>
+                <option value="Data Science">Data Science</option>
+                <option value="Web Development">Web Development</option>
+                <option value="Ethical Hacking">Ethical Hacking</option>
+                <option value="Block Chain Design">Block Chain Design</option>
+                <option value="Mobile App Developer">Mobile App Developer</option>
+                <option value="UI/UX Designing">UI/UX Designing</option>
+                <option value="Cyber Security">Cyber Security</option>
+                <option value="Others">Others</option>
+              </select>
+            </div>
+            <button className='w-full bg-[black] text-white py-2 px-4 rounded-md active:bg-[#3a3a3a] transition cursor-pointer'>Create</button>
+
+          </form>
+        
+
+      </div>
       
     </div>
   )
